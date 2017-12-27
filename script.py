4d1d3b998b8d431e6007896f402085f21786d4f7
a-ashbeck <awashbeck@gmail.com>
2017-12-27 15:34:03 -0600

    Add notes to script

--- script.py
+++ script.py
@@ -1,20 +1,26 @@
+# Import tweepy, which interfaces with the Twitter API
 import tweepy
+# Import textblo, which offers text analysis tools
 from textblob import TextBlob
 
-consumer_key = 'LsuODLkhMOuAeemlxdzCuCJzX'
-consumer_secret = 'pbrnWP0TRTW3lcrfu5c8qnr4GuRUKoRFMLFyQu04jwXskZozNE'
+# Twitter API access keys and tokens
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
-access_token = '1533962989-TeeRTMwI7coW8N53O4mhqFKvOfJ6OSQScFPgpKm'
-access_token_secret = 'duF5lMsu6QPrrS3m0CvOoSKTnxLzcK5D6z66FNFuAqkmW'
-
+# Setting tweepy up with API access info
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
+# Setting the search term
 public_tweets = api.search('Taxes')
 
+# Loop to grap tweets and analyze text
 for tweet in public_tweets:
     print(tweet.text)
     analysis = TextBlob(tweet.text)
+    print(analysis.tags)
+    print(analysis.words)
     print(analysis.sentiment)
